a4f6dfc4645b0cc8a592d85fd5a9e622a404820d
Cadrew <canino.ycan@gmail.com>
2021-07-26 20:50:28 +0200

    New rules

--- lib/rule.go
+++ lib/rule.go
@@ -1,14 +1,42 @@
 package autolunar
 
+import (
+	"fmt"
+	"errors"
+)
+
 type Rule struct {
+	model      string
 	dimensions int
 	neighbors  int
+	birth      []int
+	survive    []int
+	states     []int
 }
 
-func CreateRule(dim, n int) *Rule {
+func ReadRule(name string) (*Rule, error) {
+	if (name == "") {
+		return nil, errors.New(fmt.Sprintf("[autolunar] cannot create rule %s", name))
+	}
+	
 	return &Rule{
-		dimensions: dim,
-		neighbors: n,
+		model: "",
+		dimensions: 0,
+		neighbors: 0,
+		birth: nil,
+		survive: nil,
+		states: nil,
+	}, nil
+}
+
+func CreateEmptyRule(name string) *Rule {	
+	return &Rule{
+		model: "",
+		dimensions: 0,
+		neighbors: 0,
+		birth: nil,
+		survive: nil,
+		states: nil,
 	}
 }
 
@@ -20,10 +48,42 @@ func (r *Rule) SetNeighbors(n int) {
 	r.neighbors = n
 }
 
+func (r *Rule) SetModel(model string) {
+	r.model = model
+}
+
+func (r *Rule) SetBirth(birth []int) {
+	r.birth = birth
+}
+
+func (r *Rule) SetSurvive(survive []int) {
+	r.survive = survive
+}
+
+func (r *Rule) SetStates(states []int) {
+	r.states = states
+}
+
 func (r *Rule) GetDimensions() int {
 	return r.dimensions
 }
 
 func (r *Rule) GetNeighbors() int {
 	return r.neighbors
+}
+
+func (r *Rule) GetModel() string {
+	return r.model
+}
+
+func (r *Rule) GetBirth() []int {
+	return r.birth
+}
+
+func (r *Rule) GetSurvive() []int {
+	return r.survive
+}
+
+func (r *Rule) GetStates() []int {
+	return r.states
 }
