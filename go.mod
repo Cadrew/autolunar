f434594f75e1da62a0b344210eb830410029763a
Cadrew <canino.ycan@gmail.com>
2021-12-20 14:24:51 +0100

    go mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Cadrew/autolunar
+
+go 1.17
--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"bufio"
 	"strconv"
 
-	autolunar "./lib"
+	autolunar "github.com/Cadrew/autolunar/lib"
 )
 
 // usage example
 func main() {
 	al := autolunar.CreateGenerator()
 	err := al.SetDefault()
-	if (err != nil) {
+	if err != nil {
 		fmt.Println(err)
 		return
 	}
@@ -23,19 +23,19 @@ func main() {
 		RNG[i] = al.Rand(0, 100000)
 	}
 	fmt.Println("Random generated number:", RNG)
- 
+
 	file, err := os.OpenFile("numbers.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
- 
+
 	if err != nil {
 		fmt.Printf("failed creating file: %s", err)
 	}
- 
+
 	datawriter := bufio.NewWriter(file)
- 
+
 	for _, data := range RNG {
 		_, _ = datawriter.WriteString(strconv.Itoa(data) + "\n")
 	}
- 
+
 	datawriter.Flush()
 	file.Close()
 }
